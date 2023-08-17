183ed8487621a9e560f26727ac177138f9bcd232
ridhoauliama97 <ridhoauliama97@gmail.com>
2023-08-17 20:10:17 +0700

    fix portfolio without desc

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../assets/Projects/leaf.png";
-import emotion from "../../assets/Projects/emotion.png";
-import editor from "../../assets/Projects/codeEditor.png";
-import chatify from "../../assets/Projects/chatify.png";
-import suicide from "../../assets/Projects/suicide.png";
-import bitsOfCode from "../../assets/Projects/blog.png";
+import calculator from "../../assets/Projects/calculator-app.png";
+import creditCard from "../../assets/Projects/credit-card-vue.png";
+import formLogin from "../../assets/Projects/login-form.png";
+import parallax from "../../assets/Projects/parallax.png";
+import tictactoe from "../../assets/Projects/TicTacToe.png";
+import todo from "../../assets/Projects/todo-app.png";
 
 function Projects() {
   return (
@@ -20,68 +20,68 @@ function Projects() {
         <p style={{ color: "white" }}>Here are a few projects I've worked on recently.</p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
-            {/* <ProjectCard
-              imgPath={chatify}
+            <ProjectCard
+              imgPath={parallax}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
-            /> */}
+              title="Parallax"
+              // description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
+              ghLink="https://github.com/ridhoauliama97/parallax-portfolio"
+              // demoLink="https://plant49-ai.herokuapp.com/"
+            />
           </Col>
 
           <Col md={4} className="project-card">
-            {/* <ProjectCard
-              imgPath={bitsOfCode}
+            <ProjectCard
+              imgPath={creditCard}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
-            /> */}
+              title="Credit Card Using VueJS"
+              // description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
+              ghLink="https://github.com/ridhoauliama97/credit-card-vuejs"
+              // demoLink="https://blogs.soumya-jit.tech/"
+            />
           </Col>
 
           <Col md={4} className="project-card">
-            {/* <ProjectCard
-              imgPath={editor}
+            <ProjectCard
+              imgPath={formLogin}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"
-            /> */}
+              title="Form Login"
+              // description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+              ghLink="https://github.com/ridhoauliama97/login-form"
+              // demoLink="https://editor.soumya-jit.tech/"
+            />
           </Col>
 
           <Col md={4} className="project-card">
-            {/* <ProjectCard
-              imgPath={leaf}
+            <ProjectCard
+              imgPath={calculator}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            /> */}
+              title="Calculator App"
+              // description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              ghLink="https://github.com/ridhoauliama97/calculator-app"
+              // demoLink="https://chatify-49.web.app/"
+            />
           </Col>
 
           <Col md={4} className="project-card">
-            {/* <ProjectCard
-              imgPath={suicide}
+            <ProjectCard
+              imgPath={tictactoe}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-            /> */}
+              title="Tic Tac Toe Game"
+              // description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
+              ghLink="https://github.com/ridhoauliama97/tictactoe-reactJS"
+            />
           </Col>
 
           <Col md={4} className="project-card">
-            {/* <ProjectCard
-              imgPath={emotion}
+            <ProjectCard
+              imgPath={todo}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-            /> */}
+              title="To Do App"
+              // description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
+              // Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
+              ghLink="https://github.com/ridhoauliama97/todo-app"
+            />
           </Col>
         </Row>
       </Container>
